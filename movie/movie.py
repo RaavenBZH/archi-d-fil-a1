dce7abc76193173f5816d54e1a85692dc0699d97
RaavenBZH <emalivet56@gmail.com>
2024-09-27 10:30:44 +0200

    update movie.py

--- movie/movie.py
+++ movie/movie.py
@@ -8,17 +8,57 @@ app = Flask(__name__)
 PORT = 3200
 HOST = '0.0.0.0'
 
-with open('{}/databases/movies.json'.format("."), 'r') as jsf:
+def write(movies):
+    with open('movie/databases/movies.json'.format("."), 'w') as f:
+        json.dump(movies, f)
+
+with open('movie/databases/movies.json'.format("."), 'r') as jsf:
    movies = json.load(jsf)["movies"]
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:black'>Welcome to the Movie service!</h1>",200)
 
-@app.route("/json", methods=["GET"])
+@app.route("/json", methods=['GET'])
 def get_json():
     res = make_response(jsonify(movies), 200)
+    return res
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie),200)
+            return res
+    return make_response(jsonify({"error":"Movie ID not found"}),400)
+
+@app.route("/moviesbytitle", methods=['GET'])
+def get_movie_bytitle():
+    json = ""
+    if request.args:
+        req = request.args
+        for movie in movies:
+            if str(movie["title"]) == str(req["title"]):
+                json = movie
+
+    if not json:
+        res = make_response(jsonify({"error":"movie title not found"}),400)
+    else:
+        res = make_response(jsonify(json),200)
+    return res
+
+@app.route("/addmovie/<movieid>", methods=['POST'])
+def add_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            return make_response(jsonify({"error":"movie ID already exists"}),409)
+
+    movies.append(req)
+    write(movies)
+    res = make_response(jsonify({"message":"movie added"}),200)
     return res
 
 if __name__ == "__main__":
