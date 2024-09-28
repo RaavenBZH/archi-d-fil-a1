2ff8b46d7af6d10d14f7da87502fb11c32b83844
Baptiste <baptiste.bayche@gmail.com>
2024-09-28 13:38:49 +0200

    add showtime

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -7,12 +7,25 @@ app = Flask(__name__)
 PORT = 3202
 HOST = '0.0.0.0'
 
-with open('{}/databases/times.json'.format("."), "r") as jsf:
+with open('showtime/databases/times.json'.format("."), "r") as jsf:
    schedule = json.load(jsf)["schedule"]
 
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+@app.route("/showtimes", methods=['GET'])
+def get_schedule():
+    res = make_response(jsonify(schedule), 200)
+    return res
+
+@app.route("/showtimes/<date>", methods=['GET'])
+def get_movies_bydate(date):
+    for s in schedule:
+        if str(s["date"]) == str(date):
+            res = make_response(jsonify(s),200)
+            return res
+    return make_response(jsonify({"error":"Schedule date not found"}),400)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
