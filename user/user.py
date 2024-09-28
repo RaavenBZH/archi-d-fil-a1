514ce2698309e1a201c9ce0c7231e97b19831119
RaavenBZH <emalivet56@gmail.com>
2024-09-28 18:28:53 +0200

    users/movies/userid

--- user/user.py
+++ user/user.py
@@ -48,6 +48,47 @@ def get_user_byname(username):
          return res
    return make_response(jsonify({"error":"User name not found"}),400)
 
+@app.route("/users/movies/<userid>", methods=['GET'])
+def get_user_movies_byid(userid):
+   for user in users:
+      if str(user["id"]) == str(userid):
+         infos = []
+
+         # Get movie and date with booking service
+         try:
+            bookings = requests.get(f"http://booking:3201/bookings/{userid}").json()
+            if bookings["userid"] == userid:
+               dates = bookings["dates"]
+               for date in dates:
+                  movies = []
+                  for movie in date["movies"]:
+                     movies.append(movie)
+                  obj = {date["date"]: movies}
+                  infos.append(obj)
+         except requests.exceptions.RequestException as e:
+            print(f"Error connecting to booking service: {e}")
+            return make_response(jsonify({"error": "Error connecting to booking service"}), 500)
+
+         # Get movie data
+         for obj in infos:
+            for date, movies in obj.items():
+               print(date)
+               movies_by_date = []
+               for movie in movies:
+                  try:
+                     print(f"http://movie:3200/movies/{movie}")
+                     movie_data = requests.get(f"http://movie:3200/movies/{movie}").json()
+                     movies_by_date.append(movie_data)
+                  except requests.exceptions.RequestException as e:
+                     print(f"Error connecting to movie service: {e}")
+                     return make_response(jsonify({"error": "Error connecting to movie service"}), 500)
+
+               obj[date] = movies_by_date
+
+         res = make_response(jsonify(user),200)
+         return res
+   return make_response(jsonify({"error": "User ID not found"}), 400)
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
