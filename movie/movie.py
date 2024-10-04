3a210c6da0daceb006b9f240da8065a570227e9b
Baptiste <baptiste.bayche@gmail.com>
2024-10-04 11:47:11 +0200

    delete_movie_with_id

--- movie/movie.py
+++ movie/movie.py
@@ -18,7 +18,7 @@ query.set_field('movie_with_id', r.movie_with_id)
 query.set_field('movie_with_title', r.movie_with_title)
 query.set_field('movie_with_director', r.movie_with_director)
 query.set_field('movie_with_rate', r.movie_with_rate)
-
+query.set_field('delete_movie_with_id', r.delete_movie_with_id)
 schema = make_executable_schema(type_defs, movie, query)
 
 # root message
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -33,3 +33,13 @@ def movie_with_rate(_, info, _rate):
             if cond1 and cond2:
                 result_set.append(movie)
     return result_set
+
+def delete_movie_with_id(_,info,_id):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movies["movies"].remove(movie)
+                with open('{}/data/movies.json'.format("."), 'w') as f:
+                    json.dump(movies, f)
+                return movie
