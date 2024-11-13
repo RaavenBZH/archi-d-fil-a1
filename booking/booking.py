ae8d35b02bf6c21b67ba705c401aacdf2d17772c
Baptiste <baptiste.bayche@gmail.com>
2024-11-13 21:31:34 +0100

    add booking with grpc endpoints

--- booking/booking.py
+++ booking/booking.py
@@ -1,101 +1,104 @@
-# import grpc
-# from concurrent import futures
-# import booking_pb2
-# import booking_pb2_grpc
+import grpc
+from concurrent import futures
+import booking_pb2
+import booking_pb2_grpc
 import json
 
-# class BookingServicer(booking_pb2_grpc.BookingServicer):
+class BookingService(booking_pb2_grpc.BookingService):
 
-#     def __init__(self):
-#         with open('{}/data/bookings.json'.format("."), "r") as jsf:
-#             self.db = json.load(jsf)["schedule"]
+   def __init__(self):
+      with open('{}/data/bookings.json'.format("."), "r") as jsf:
+         self.db = json.load(jsf)["bookings"]
 
-# def serve():
-#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-#     booking_pb2_grpc.add_BookingServicer_to_server(BookingServicer(), server)
-#     server.add_insecure_port('[::]:3002')
-#     server.start()
-#     server.wait_for_termination()
+   def Home(self, request, context):
+        print("Home")
+        response = booking_pb2.HomeResponse()
+        response.message = "<h1>Bienvenue sur booking</h1>"
+        return response
 
-#     with open('./databases/bookings.json'.format("."), "r") as jsf:
+   def GetAllBookings(self, request, context):
+      print("GetAllBookings")
+      response = booking_pb2.AllBookingsResponse()
+      for booking in self.db:
+         booking_user = booking_pb2.BookingsUser(
+               userid=booking["userid"],
+               dates=[
+                  booking_pb2.DateItem(
+                     date=date["date"],
+                     movies=date["movies"]
+                  ) for date in booking["dates"]
+               ]
+         )
+         response.bookings.append(booking_user)
+      print(response)
+      return response
 
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
-import json
-from werkzeug.exceptions import NotFound
+   def GetBookingsForUser(self, request, context):
+      print("GetBookingsForUser")
+      response = booking_pb2.BookingsUserResponse()
+      for booking in self.db:
+         if booking["userid"] == request.userid:
+               response.userid = booking["userid"]
+               for date in booking["dates"]:
+                  date_item = booking_pb2.DateItem(
+                     date=date["date"],
+                     movies=date["movies"]
+                  )
+                  response.dates.append(date_item)
+               break
+      return response
 
-app = Flask(__name__)
+   def AddBookingForUser(self, request, context):
+      new_booking = {
+         "userid": request.userid,
+         "date": request.date,
+         "movies": [request.movieid]
+      }
 
-PORT = 3201
-HOST = '0.0.0.0'
+      # Rechercher l'utilisateur dans les données existantes
+      user_found = False
+      for booking in self.db:
+         if booking["userid"] == request.userid:
+               # Ajouter la date ou mettre à jour les films pour cette date
+               for date in booking["dates"]:
+                  if date["date"] == request.date:
+                     date["movies"].append(request.movieid)
+                     break
+               else:
+                  # Si la date n'existe pas encore, l'ajouter
+                  booking["dates"].append(new_booking)
+               user_found = True
+               break
 
-with open('./data/bookings.json'.format("."), "r") as jsf:
-   bookings = json.load(jsf)["bookings"]
+      # Si l'utilisateur n'a pas été trouvé, créer une nouvelle réservation
+      if not user_found:
+         self.db.append({
+               "userid": request.userid,
+               "dates": [new_booking]
+         })
 
-@app.route("/", methods=['GET'])
-def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+      # Préparer la réponse
+      response = booking_pb2.BookingsUserResponse(userid=request.userid)
+      for booking in self.db:
+         if booking["userid"] == request.userid:
+               for date in booking["dates"]:
+                  date_item = booking_pb2.DateItem(
+                     date=date["date"],
+                     movies=date["movies"]
+                  )
+                  response.dates.append(date_item)
+               break
+      return response
 
-@app.route("/bookings", methods=['GET'])
-def get_json():
-    res = make_response(jsonify(bookings), 200)
-    return res
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    booking_pb2_grpc.add_BookingServiceServicer_to_server(BookingService(), server)
+    server.add_insecure_port('[::]:3003')
+    server.start()
+    server.wait_for_termination()
 
-@app.route("/bookings/<userid>", methods=['GET'])
-def get_booking_for_user(userid):
-    for booking in bookings:
-        if str(booking["userid"]) == str(userid):
-            res = make_response(jsonify(booking),200)
-            return res
-    return make_response(jsonify({"error":"User booking not found"}),400)
-
-@app.route("/bookings/<userid>", methods=['POST'])
-def add_booking_byuser(userid):
-   new_booking  = request.get_json()
-
-   url = f"http://showtime:3202/showtimes/{new_booking["date"]}"
-   showtime = requests.get(url).json()
-
-   if "error" not in showtime and new_booking["movieid"] in showtime["movies"]:
-      for booking in bookings:
-         if booking["userid"] == userid: 
-               for booking_date in booking["dates"]:
-                  if booking_date["date"] == new_booking["date"]:  
-                     for movie in booking_date["movies"]:
-                           if movie == new_booking["movieid"]:
-                              return make_response(jsonify({"error": "User already booked this movie"}), 400)
-                     booking_date["movies"].append(new_booking["movieid"])
-                     write(bookings)  
-                     return make_response(jsonify({"message": "Booking updated successfully"}), 200)
-               booking["dates"].append({
-                  "date": new_booking["date"],
-                  "movies": [new_booking["movieid"]]
-               })
-               write(bookings) 
-               return make_response(jsonify({"message": "New date added and movie booked successfully"}), 200)
-
-      bookings.append({
-         "userid": userid,
-         "dates": [{
-               "date": new_booking["date"],
-               "movies": [new_booking["movieid"]]
-         }]
-      })
-      write(bookings)  # Sauvegarde des modifications dans le fichier JSON
-      return make_response(jsonify({"message": "New user and booking created successfully"}), 200)
-   else:
-      return make_response(jsonify({"error": "booking as not a showtime"}), 400)
-
-
-def write(bookings):
-    with open('./databases/bookings.json'.format("."), 'w') as f:
-        json.dump(bookings, f)
+        
+if __name__ == '__main__':
+    serve()
 
 
-if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
-
-
-# if __name__ == '__main__':
-#     serve()
